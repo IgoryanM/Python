720546be4afc30968c2659a4d0369b472f79a09f
IgoryanM <69872101+IgoryanM@users.noreply.github.com>
2020-09-20 18:59:36 +0300

    Update 7_2.py

--- 7_2.py
+++ 7_2.py
@@ -4,20 +4,30 @@ class Clothes:
 
 
 class Coat(Clothes):
-    def __init__(self, v):
-        super().__init__()
+    def __init__(self, name, v):
+        super().__init__(name)
         self.v = v
 
     def consumption_coat(self):
-        print(f'Расход ткани на {self.name} - {self.v / 6.5 + 0.5}')
+        print(f'Расход ткани на пальто {self.name} - {self.v // 6.5 + 0.5}')
+        return (self.v // 6.5 + 0.5)
 
 
 class Costume(Clothes):
-    def __init__(self, h):
-        super().__init__()
+    def __init__(self, name, h):
+        super().__init__(name)
         self.h = h
 
     def consumption_costume(self):
-        print(f'Расход ткани на {self.name} - {2 * self.h + 0.3}')
+        print(f'Расход ткани на костюм {self.name} - {2 * self.h + 0.3}')
+        return (2 * self.h + 0.3)
 
-new_costume = Costume('q', 1)
+
+new_costume = Costume('Armani', 100)
+new_coat = Coat('Gucci', 200)
+
+#new_costume.consumption_costume()
+#new_coat.consumption_coat()
+
+print(f'Полный расход ткани - {new_costume.consumption_costume() + new_coat.consumption_coat()}')
+
