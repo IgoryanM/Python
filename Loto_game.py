460c61dcddb39eee13865137873e7a30d653c209
IgoryanM <69872101+IgoryanM@users.noreply.github.com>
2020-09-30 23:23:21 +0300

    Add files via upload

---
+++ Loto_game.py
@@ -0,0 +1,81 @@
+import random
+
+
+class LotoCard:
+    def __init__(self):
+        self.new_card = []
+        for i in range(3):
+            line = random.sample(range(1, 91), 5)
+            line.sort()
+
+            for i in range(0, 4):
+                line.insert((random.randint(0, 5)), '-')
+
+            self.new_card.append(line)
+
+    def __str__(self):
+        return ('\n'.join(' '.join([str(el) for el in line]) for line in self.new_card))
+
+    def win(self):
+        iswin = True
+        for line in self.new_card:
+            st_line = str(line)
+            for el in st_line:
+                if el.isdigit() == True:
+                    iswin = False
+        return iswin
+
+
+class Game:
+    def __init__(self):
+        self.bag = [el for el in range(1, 91)]
+
+    def turn(self):
+        random.shuffle(self.bag)
+        return self.bag.pop()
+
+    def start(self, human_card, pc_card):
+        while self.bag != []:
+
+            num = self.turn()
+
+            print(
+                f'Новый бочонок: {num} осталось {len(self.bag)}\n----Ваша карточка----\n{human_card}\n---------------------\n-Карточка компьютера-\n{pc_card}\n---------------------')
+
+            if human_card.win():
+                print('\n★★★Победил игрок★★★')
+                break
+            if pc_card.win():
+                print('\n★★★Победил компьютер★★★')
+                break
+
+            for line in pc_card.new_card:
+                if num in line:
+                    line[line.index(num)] = '•'
+
+            my_turn = input('\nЗачеркнуть цифру? (y/n) ')
+            selector = False
+            if my_turn == 'y':
+                for line in human_card.new_card:
+                    if num in line:
+                        line[line.index(num)] = '•'
+                        selector = True
+            if my_turn == 'n':
+                for line in human_card.new_card:
+                    if num not in line:
+                        selector = True
+                    else:
+                        selector = False
+
+            if selector == False:
+                print('Вы ошиблись\n---Игрок проиграл---')
+                break
+
+        if input('Играть ещё? (y/n): ') == 'y':
+            self.start(LotoCard(), LotoCard())
+
+hm_card = LotoCard()
+cpu_card = LotoCard()
+st = Game()
+
+st.start(hm_card, cpu_card)
